e143b2d840dac06e8fdb51c9d6db74b949ebe480
Douglas Costa <douglaspcosta15@gmail.com>
2025-11-18 02:32:48 -0300

    att versao mobile

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
- let count = 1;
-document.getElementById("radio1").checked = true;
+ let count = 1;         //variavel
 
+document.getElementById("radio1").checked = true;   /*marca o radio button 1 
+                                                    como selecionado quando a página carrega*/
 setInterval( function(){
-    nextImage();
+    nextImage();                                    //chama a função nextImage() a cada 5 seg
 }, 5000)
 
 
-function nextImage(){
-    count++;
-    if(count>3){
-        count = 1;
+function nextImage(){                                            //funçao princial pra mudar slide
+    count++;                                                     //aumenta em 1 o contador
+    if(count>3){            
+        count = 1;                                              //loop infinito      
     }
 
-    document.getElementById("radio"+count).checked = true;
+    document.getElementById("radio"+count).checked = true;      //conecta o JavaScript ao HTML
 }
